9b0396acf904833c0057466e51486a1c547aa97f
athomaj <julien@athomas.io>
2024-11-08 10:19:49 +0100

    fddf

--- back/app.js
+++ back/app.js
@@ -38,7 +38,9 @@ app.use(express.static(path.join(__dirname, 'public')));
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
 
-app.get('*', (req, res) => { res.sendFile(path.join(__dirname, ‘public’, ‘index.html’)); });
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname, 'public', 'index.html'));
+});
 
 
 
