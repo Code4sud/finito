3742f01834461c959c20392c355b8cdf01e0a208
cei35 <hackingtoolsservice@gmail.com>
2024-11-08 10:14:38 +0100

    ajout header conforme a la charte graphique

--- src/pages/AlertPage.tsx
+++ src/pages/AlertPage.tsx
@@ -78,7 +78,7 @@ export default function AlertPage() {
   }, []);
 
   return (
-    <main className="container mx-auto px-4 py-8">
+    <main className="container mx-auto px-4 py-8 mt-16">
       <div className="grid grid-cols-1 lg:grid-cols-3 gap-8">
         <div className="lg:col-span-2">
           <div className="bg-white rounded-xl shadow-lg p-6 mb-8">
